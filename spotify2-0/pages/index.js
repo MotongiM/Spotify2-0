b63b45d6f43518c0ee7a6d98c82e3289c9b16fa2
Michel <Michel_motongi@hotmail.com>
2022-06-15 13:17:39 -0400

    Setting up project

---
+++ spotify2-0/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>This is dope</h1>
+      <main>
+        {/* SideBar */}
+        {/* Center */}
+      </main>
+      <div>{/* Player */}</div>
+    </div>
+  )
+}
