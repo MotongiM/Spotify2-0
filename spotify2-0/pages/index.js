e3ec5dd4c0ea62dbcfc9e3ba9ef8cecb572206dc
Michel <Michel_motongi@hotmail.com>
2022-06-15 15:58:43 -0400

    Add background to the app

--- spotify2-0/pages/index.js
+++ spotify2-0/pages/index.js
@@ -1,13 +1,15 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Sidebar from '../components/Sidebar'
+import Center from '../components/Center'
+
 
 export default function Home() {
   return (
-    <div className="">
-      <h1>This is dope</h1>
-      <main>
-        {/* SideBar */}
-        {/* Center */}
+    <div className="bg-black h-screen overflow-hidden">
+      <main className=''>
+        <Sidebar />
+        {/* <Center /> */}
       </main>
       <div>{/* Player */}</div>
     </div>
