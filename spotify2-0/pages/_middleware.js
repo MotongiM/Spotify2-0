e5526d13c690dfd87325bcbc4b4f7d8dbd61d297
Michel <Michel_motongi@hotmail.com>
2022-06-24 02:44:54 -0400

    Fix issues where the URL could not be detected

--- spotify2-0/pages/_middleware.js
+++ spotify2-0/pages/_middleware.js
@@ -1,6 +1,18 @@
 import { getToken } from "next-auth/jwt";
-import { NextResponse }from "next/server";
+
+import { NextResponse } from "next/server";
 
 export async function middleware(req) {
+    
     const token = await getToken({ req, secret: process.env.JWT_SECRET });
+
+    const { pathname , origin  } = req.nextUrl;
+
+    if (pathname.includes("/api/auth") || token ) {
+        return NextResponse.next();
+    }
+
+    if (!token && pathname !== `/Login`) {
+        return NextResponse.rewrite(`${origin}/Login`);
+    }
 }
